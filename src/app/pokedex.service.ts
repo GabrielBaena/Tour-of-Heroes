dc7eb4f129042f8ea35b89310bff4551d192084a
Gabriel Baena Gazda <gabriel.gazda@grupojacto.com.br>
2023-03-29 10:45:39 -0300

    pokemon specie fetched

--- src/app/pokedex.service.ts
+++ src/app/pokedex.service.ts
@@ -6,8 +6,6 @@ import { catchError, tap } from 'rxjs';
 
 import { Observable, of, map } from 'rxjs';
 
-import { Pokemon, pokemon, pokemonData} from './pokemon';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -28,73 +26,80 @@ export class PokedexService {
   gen_1_list:[][] = [];
 
   getdata(id:any) {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon/${id}`)
       .pipe(
         tap(),
         catchError(this.handleError('getPokemon', []))
       )
   }
 
+  getpokemospecies(url:string) {
+    return  this._http.get<{}>(url)
+      .pipe(
+        tap(),
+        catchError(this.handleError('getPokemonSpecies', []))
+      )
+  }
 
 
   getgen1() {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon?limit=151`)
       .pipe(
         tap(_ => this.log(`fetched Kanto's pokemon`)),
         catchError(this.handleError('getPokemon', []))
       )
   }
   getgen2() {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=151`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=151`)
       .pipe(
         tap(_ => this.log(`fetched Johto's pokemon`)),
         catchError(this.handleError('getPokemon', []))
       )
   }
   getgen3() {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon?limit=135&offset=251`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon?limit=135&offset=251`)
       .pipe(
         tap(_ => this.log(`fetched Hoenn's pokemon`)),
         catchError(this.handleError('getPokemon', []))
       )
   }
   getgen4() {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon?limit=107&offset=386`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon?limit=107&offset=386`)
       .pipe(
         tap(_ => this.log(`fetched Sinnoh's pokemon`)),
         catchError(this.handleError('getPokemon', []))
       )
   }
   getgen5() {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon?limit=156&offset=493`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon?limit=156&offset=493`)
       .pipe(
         tap(_ => this.log(`fetched Unova's pokemon`)),
         catchError(this.handleError('getPokemon', []))
       )
   }
   getgen6() {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon?limit=72&offset=649`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon?limit=72&offset=649`)
       .pipe(
         tap(_ => this.log(`fetched Kalos' pokemon`)),
         catchError(this.handleError('getPokemon', []))
       )
   }
   getgen7() {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon?limit=88&offset=721`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon?limit=88&offset=721`)
       .pipe(
         tap(_ => this.log(`fetched Alola's pokemon`)),
         catchError(this.handleError('getPokemon', []))
       )
   }
   getgen8() {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon?limit=96&offset=809`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon?limit=96&offset=809`)
       .pipe(
         tap(_ => this.log(`fetched Galar's pokemon`)),
         catchError(this.handleError('getPokemon', []))
       )
   }
   getgen9() {
-    return  this._http.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon?limit=105&offset=905`)
+    return  this._http.get<{}>(`https://pokeapi.co/api/v2/pokemon?limit=105&offset=905`)
       .pipe(
         tap(_ => this.log(`fetched Paldea's pokemon`)),
         catchError(this.handleError('getPokemon', []))
--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -16,9 +16,6 @@ export class PokemonDetailComponent {
 
     private _pokedexsService:PokedexService) {}
   
-  next: number = 2
-  image_url = ''
-  moves:any[] = []
   pokemon: any[] = []
   ngOnInit(): void{
     const name = String(this.route.snapshot.paramMap.get('name'));
@@ -28,6 +25,12 @@ export class PokemonDetailComponent {
         return a.version_group_details[0].level_learned_at - b.version_group_details[0].level_learned_at;
       })
         this.pokemon.push(pokemon)
+      console.log(this.pokemon[0].species.url)
+      this._pokedexsService.getpokemospecies(this.pokemon[0].species.url)
+      .subscribe((specie:any) => {
+        this.pokemon.push(specie)  
+        console.log(this.pokemon)
+    })
       }
       );
   }
